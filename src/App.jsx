5f6a8d9e5803b5787e6f6c5ea5d0cfda00d110ea
Rosalía Matzumiya <15949638+rosalia-matzumiya@users.noreply.github.com>
2026-03-16 16:40:15 -0600

    Feature: render archeological sites from JSON dataset

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
+import SiteList from './components/SitesList'
 import './App.css'
+
 
 function App() {
 
   return (
     <div>
       <h1>Mexico Archaeology Explorer</h1>
+      <SiteList />
     </div>
   )
 }
